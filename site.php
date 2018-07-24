ae1c345798953f96b1c5d451b018c41c8047dc9e
toledoneto <toledo.neto@outlook.com>
2018-07-24 17:27:34 -0300

    122. Site - Meus Pedidos

--- site.php
+++ site.php
@@ -185,76 +185,77 @@ $app->get("/checkout", function(){
 
 });
 
+
 $app->post("/checkout", function(){
-
+	
 	User::verifyLogin(false);
-
+	
 	if (!isset($_POST['zipcode']) || $_POST['zipcode'] === '') {
 		Address::setMsgError("Informe o CEP.");
 		header('Location: /checkout');
 		exit;
 	}
-
+	
 	if (!isset($_POST['desaddress']) || $_POST['desaddress'] === '') {
 		Address::setMsgError("Informe o endereço.");
 		header('Location: /checkout');
 		exit;
 	}
-
+	
 	if (!isset($_POST['desdistrict']) || $_POST['desdistrict'] === '') {
 		Address::setMsgError("Informe o bairro.");
 		header('Location: /checkout');
 		exit;
 	}
-
+	
 	if (!isset($_POST['descity']) || $_POST['descity'] === '') {
 		Address::setMsgError("Informe a cidade.");
 		header('Location: /checkout');
 		exit;
 	}
-
+	
 	if (!isset($_POST['desstate']) || $_POST['desstate'] === '') {
 		Address::setMsgError("Informe o estado.");
 		header('Location: /checkout');
 		exit;
 	}
-
+	
 	if (!isset($_POST['descountry']) || $_POST['descountry'] === '') {
 		Address::setMsgError("Informe o país.");
 		header('Location: /checkout');
 		exit;
 	}
-
+	
 	$user = User::getFromSession();
-
+	
 	$address = new Address();
-
+	
 	$_POST['deszipcode'] = $_POST['zipcode'];
+	
 	$_POST['idperson'] = $user->getidperson();
-
+	
 	$address->setData($_POST);
-
+	
 	$address->save();
-
+	
 	$cart = Cart::getFromSession();
-
-	$totals = $cart->getCalculateTotal();
-
+	
+	$cart->getCalculatedTotal();
+	
 	$order = new Order();
-
+	
 	$order->setData([
 		'idcart'=>$cart->getidcart(),
 		'idaddress'=>$address->getidaddress(),
 		'iduser'=>$user->getiduser(),
 		'idstatus'=>OrderStatus::EM_ABERTO,
-		'vltotal'=>$totals['vlprice'] + $cart->getvlfreight()
+		'vltotal'=>$cart->getvltotal()
 	]);
-
+	
 	$order->save();
-
+	
 	header("Location: /order/".$order->getidorder());
 	exit;
-
 });
 
 $app->get("/login", function(){
@@ -497,6 +498,7 @@ $app->get("/boleto/:idorder", function($idorder){
 	$taxa_boleto = 5.00;
 	$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
 	$valor_cobrado = formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+	$valor_cobrado = str_replace(".", "", $valor_cobrado);
 	$valor_cobrado = str_replace(",", ".",$valor_cobrado);
 	$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
 	$dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero - REGRA: Máximo de 8 caracteres!
@@ -540,6 +542,42 @@ $app->get("/boleto/:idorder", function($idorder){
 	$path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "boletophp" . DIRECTORY_SEPARATOR . "include" . DIRECTORY_SEPARATOR;
 	require_once($path . "funcoes_itau.php");
 	require_once($path . "layout_itau.php");
+});
+
+$app->get("/profile/orders", function(){
+	
+	User::verifyLogin(false);
+	
+	$user = User::getFromSession();
+	
+	$page = new Page();
+	
+	$page->setTpl("profile-orders", [
+		'orders'=>$user->getOrders()
+	]);
+});
+
+$app->get("/profile/orders/:idorder", function($idorder){
+	
+	User::verifyLogin(false);
+	
+	$order = new Order();
+	
+	$order->get((int)$idorder);
+	
+	$cart = new Cart();
+	
+	$cart->get((int)$order->getidcart());
+	
+	$cart->getCalculatedTotal();
+	
+	$page = new Page();
+	
+	$page->setTpl("profile-orders-detail", [
+		'order'=>$order->getValues(),
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()
+	]);	
 });
 
  ?>
