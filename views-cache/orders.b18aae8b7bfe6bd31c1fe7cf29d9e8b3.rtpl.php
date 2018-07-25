0e4738e51a9c6a8fb0211a6430afc9ed9481c3e8
toledoneto <toledo.neto@outlook.com>
2018-07-25 13:41:42 -0300

    128. Admin - Pedidos Paginação e Busca

--- views-cache/orders.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
+++ views-cache/orders.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
@@ -15,8 +15,21 @@
 <section class="content">
 
   <div class="row">
-  	<div class="col-md-12">
-  		<div class="box box-primary">
+    <div class="col-md-12">
+      <div class="box box-primary">
+
+          <div class="box-header">
+            <div class="box-tools">
+                <form action="/admin/orders">
+                  <div class="input-group input-group-sm" style="width: 150px;">
+                    <input type="text" name="search" class="form-control pull-right" placeholder="Search" value="<?php echo htmlspecialchars( $search, ENT_COMPAT, 'UTF-8', FALSE ); ?>">
+                    <div class="input-group-btn">
+                      <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
 
             <div class="box-body no-padding">
               <table class="table table-striped">
@@ -32,6 +45,7 @@
                 </thead>
                 <tbody>
                   <?php $counter1=-1;  if( isset($orders) && ( is_array($orders) || $orders instanceof Traversable ) && sizeof($orders) ) foreach( $orders as $key1 => $value1 ){ $counter1++; ?>
+
                   <tr>
                     <td><?php echo htmlspecialchars( $value1["idorder"], ENT_COMPAT, 'UTF-8', FALSE ); ?></td>
                     <td><?php echo htmlspecialchars( $value1["desperson"], ENT_COMPAT, 'UTF-8', FALSE ); ?></td>
@@ -45,16 +59,27 @@
                     </td>
                   </tr>
                   <?php }else{ ?>
+
                   <tr>
                       <td colspan="6">Nenhum pedido foi encontrado.</td>
                   </tr>
                   <?php } ?>
+
                 </tbody>
               </table>
             </div>
             <!-- /.box-body -->
+            <div class="box-footer clearfix">
+              <ul class="pagination pagination-sm no-margin pull-right">
+                <?php $counter1=-1;  if( isset($pages) && ( is_array($pages) || $pages instanceof Traversable ) && sizeof($pages) ) foreach( $pages as $key1 => $value1 ){ $counter1++; ?>
+
+                <li><a href="<?php echo htmlspecialchars( $value1["href"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["text"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></li>
+                <?php } ?>
+
+              </ul>
+            </div>
           </div>
-  	</div>
+    </div>
   </div>
 
 </section>
