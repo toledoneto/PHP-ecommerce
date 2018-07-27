7615947741a3fba9b9ced5e37d898c39b874c33f
toledoneto <toledo.neto@outlook.com>
2018-07-27 13:54:08 -0300

    Classe Shipping

--- vendor/hcodebr/php-classes/src/PagSeguro/Shipping.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Shipping.php
@@ -2,17 +2,63 @@
 
 namespace Hcode\PagSeguro;
 
-class Shipping
-{
+class Shipping {
 
-	const PAC = 1;
-	const SEDEX = 2;
-	const OTHER = 3;
+    const PAC = 1;
+    const SEDEX = 2;
+    const OTHER = 3;
 
-	private $address;
-	private $type;
-	private $cost;
-	private $addressRequired;
+    private $address;
+    private $type;
+    private $cost;
+    private $addressRequired;
+
+    public function __construct(
+        Address $address,
+        float $cost,
+        int $type,
+        bool $addressRequired = true
+    )
+    {
+
+        if ($type < 1 || $type > 3)
+        {
+
+            throw new Exception("Informe um tipo de frete válido.");
+
+        }
+
+        $this->address = $address;
+        $this->cost = $cost;
+        $this->type = $type;
+        $this->addressRequired = $addressRequired;
+
+    }
+
+    public function getDOMElement():DOMElement
+    {
+
+        $dom = new DOMDocument();
+
+        $shipping = $dom->createElement("shipping");
+        $shipping = $dom->appendChild($shipping);
+
+        $address = $this->address->getDOMElement();
+        $address = $dom->importNode($address, true);
+        $address = $documents->appendChild($address);
+
+        $cost = $dom->createElement("cost", number_format($this->cost, 2, ".", ""));
+        $cost = $shipping->appendChild($cost);
+
+        $type = $dom->createElement("type", $this->type);
+        $type = $shipping->appendChild($type);
+
+        $addressRequired = $dom->createElement("addressRequired", ($this->addressRequired) ? "true" : "false");
+        $addressRequired = $shipping->appendChild($addressRequired);
+
+        return $shipping;
+
+    }
 
 }
 
